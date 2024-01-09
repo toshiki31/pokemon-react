926a46aeaf37c018199abed7a7c27db584b30c78
mori toshiki <toshiki@morinoMacBook-Air.local>
2024-01-09 20:01:08 +0900

    add: step8

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import { useEffect, useState } from "react";
 import PokemonThumbnails from "./PokemonThumbnails";
 
 function App() {
-  const [pokemonNames, setPokemonNames] = useState([]);
+  const [allPokemons, setAllPokemons] = useState([]);
+  const createPokemonObject = (results) => {
+    results.forEach((pokemon) => {
+      const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
+      fetch(pokemonUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          const image = data.sprites.other["official-artwork"].front_default;
+          const type = data.types[0].type.name;
+          console.log(data.name, image, type);
+        });
+    });
+  };
 
   // 仮でデータを配列にする
   const pokemons = [
@@ -30,20 +42,21 @@ function App() {
   ];
 
   // APIからデータを取得する
-  const url = "https://pokeapi.co/api/v2/pokemon";
-  useEffect(() => {
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const getAllPokemons = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.results[0].name);
-        // 仮で3つのポケモンの名前をセットする
-        const names = [
-          data.results[0].name,
-          data.results[1].name,
-          data.results[2].name,
-        ];
-        setPokemonNames(names);
+        setAllPokemons(data.results);
+        createPokemonObject(data.results);
+
+        // 次の20件をURLにセットする
+        setUrl(data.next);
       });
+  };
+
+  useEffect(() => {
+    getAllPokemons();
   }, []);
 
   return (
@@ -55,7 +68,7 @@ function App() {
           {pokemons.map((pokemon, index) => (
             <PokemonThumbnails
               id={pokemon.id}
-              name={pokemonNames[index]}
+              name={allPokemons[index]?.name}
               image={pokemon.image}
               type={pokemon.type}
               key={index}
